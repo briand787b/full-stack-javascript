b02e42cccba86c7acb3a1932e752229b617d06ab
brian dostilio <briand787b@gmail.com>
2020-06-03 07:08:57 -0400

    using state hooks

--- frontend/react/src/pages/ArticlePage.js
+++ frontend/react/src/pages/ArticlePage.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({  upvotes: 0, comments: []});
     const { name } = useParams();
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 })
+    });
 
     const article = articles.find(article => article.name === name );
 
@@ -12,6 +17,7 @@ const ArticlePage = () => {
         ? (
         <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvotes</p>
         {article.content.map((paragraph, id) => (
             <p key={id}>{paragraph}</p>
         ))}
