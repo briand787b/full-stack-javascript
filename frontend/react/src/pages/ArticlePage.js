c640c602b4c375350adf1f8c55b53d5831ef243a
brian dostilio <briand787b@gmail.com>
2020-06-21 10:00:25 -0400

    added article page live fetching

--- frontend/react/src/pages/ArticlePage.js
+++ frontend/react/src/pages/ArticlePage.js
@@ -4,27 +4,33 @@ import articles from './article-content';
 import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({  upvotes: 0, comments: []});
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { name } = useParams();
 
     useEffect(() => {
-        setArticleInfo({ upvotes: 3 })
+        const fetchArticleInfo = async () => {
+            const result = await fetch(`http://localhost:8000/api/articles/${name}`)
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+
+        fetchArticleInfo();
     }, []);
 
-    const article = articles.find(article => article.name === name );
+    const article = articles.find(article => article.name === name);
 
-    return article 
+    return article
         ? (
-        <>
-        <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvotes</p>
-        {article.content.map((paragraph, id) => (
-            <p key={id}>{paragraph}</p>
-        ))}
-        </>
-    ) : (
-        <NotFoundPage/>
-    );
+            <>
+                <h1>{article.title}</h1>
+                <p>This article has {articleInfo.upvotes} upvotes</p>
+                {article.content.map((paragraph, id) => (
+                    <p key={id}>{paragraph}</p>
+                ))}
+            </>
+        ) : (
+            <NotFoundPage />
+        );
 }
 
 export default ArticlePage;
